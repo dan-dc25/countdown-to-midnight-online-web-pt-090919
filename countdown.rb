b3c0f6d84dac01ada9c61b2774245cf441c33806
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-15 19:07:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,9 +8,9 @@ def countdown(count)
 end
 
 def countdown_with_sleep(countdown)
-  while count > 0
-    puts "#{count} SECOND(S)!"
-    count -= 1
+  while countdown > 0
+    puts "#{countdown} SECOND(S)!"
+    countdown -= 1
     sleep 5
   end
   return "HAPPY NEW YEAR!"
